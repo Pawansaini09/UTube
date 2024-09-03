369da3c8cc4244655d6350f3913d546382196170
Pawansaini09 <mr.pawansaini09@gmail.com>
2024-09-04 00:04:45 +0530

    Done

---
+++ src/context/UtilsContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState, useContext } from "react";
+
+const UtilsContext= createContext(null);
+
+export const UtilsContextProvider =({children }) => {
+
+    const [isSidebar, setIsSidebar] = useState(false);
+    const [mobileShow, setMobileShow] = useState(false);
+
+    return (
+        <UtilsContext.Provider value={{isSidebar, setIsSidebar, mobileShow, setMobileShow }}>
+            {children}
+        </UtilsContext.Provider>
+    )
+}
+export const useUtils =() => {
+    const utilsContext = useContext( UtilsContext);
+
+    if(!UtilsContext) return null;
+
+    return UtilsContext;
+}
